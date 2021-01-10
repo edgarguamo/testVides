1bd5d2b3313b328f88c928be9e27fa707782664b
pablom7 <pamontano1998@gmail.com>
2021-01-10 09:01:05 -0500

    frontend hoteles y turismo

---
+++ vides/resources/views/turismo.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sitios Turisticos</title>
+    <link rel="stylesheet" href="../css/turismo.css">
+</head>
+<body>
+    <x-navbar/>
+    <section class="busquedalugar">
+        <div class="textodebusqueda">
+            <h2>Consulta informacion de hoteles y lugares turisticos</h2>
+            <input class="bsqd" type="text" name="hotel" size="50" height="30">
+        </div>
+        <div class="grfcs">
+            <h3>Ocupacion</h3>
+            <img src="../imgs/pastel.jpg" alt="">
+            <h3>Agosto</h3>
+        </div>
+        <div class="grfcs">
+            <h3>Tarifa promedio por habitacion</h3>
+            <img src="../imgs/barras.png" alt="">
+            <h3>Agosto</h3>
+        </div>
+        <div class="grfcs">
+            <h3>Mas información</h3>
+            <img src="../imgs/edificios.png" alt="">
+        </div>
+    </section>
+</body>
+</html>    
